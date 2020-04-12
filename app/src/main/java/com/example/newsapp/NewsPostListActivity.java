820133420d2a45c74564d778dd894fb306e41281
Amin <valiamin21@gmail.com>
2020-04-12 23:19:28 +0430

    News app created

---
+++ app/src/main/java/com/example/newsapp/NewsPostListActivity.java
@@ -0,0 +1,17 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class NewsPostListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_post_list);
+
+
+    }
+}
