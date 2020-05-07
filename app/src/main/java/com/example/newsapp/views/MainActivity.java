1fbc14546b1efed91ad6ca93dadb5c97cfc35910
Amin <valiamin21@gmail.com>
2020-05-07 17:47:58 +0430

    removing redundant toast message and some other code lines

--- app/src/main/java/com/example/newsapp/views/MainActivity.java
+++ app/src/main/java/com/example/newsapp/views/MainActivity.java
@@ -4,18 +4,15 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.example.newsapp.R;
 import com.example.newsapp.Utilities;
@@ -23,7 +20,6 @@ import com.example.newsapp.adapters.CategoryTagsRecyclerAdapter;
 import com.example.newsapp.api_services.CategoriesApiService;
 import com.example.newsapp.data_model.Category;
 import com.example.newsapp.views.fragments.NewsItemListFragment;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 import java.util.List;
 
@@ -53,7 +49,6 @@ public class MainActivity extends AppCompatActivity implements CategoryTagsRecyc
         CategoriesApiService.requestCategoriesList(this, new CategoriesApiService.OnCategoriesApiFinished() {
             @Override
             public void onResponse(List<Category> categoryList) {
-                Toast.makeText(MainActivity.this, "received", Toast.LENGTH_SHORT).show();
                 CategoryTagsRecyclerAdapter adapter = new CategoryTagsRecyclerAdapter(MainActivity.this,categoryList, MainActivity.this);
                 categoryTagsRecyclerView.setAdapter(adapter);
             }
