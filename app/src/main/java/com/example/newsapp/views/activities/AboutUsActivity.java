a80d6821367dd49afd36a9ba4dc6570abe2f3c53
Amin <valiamin21@gmail.com>
2020-06-07 13:30:00 +0430

    making about page

--- app/src/main/java/com/example/newsapp/views/activities/AboutUsActivity.java
+++ app/src/main/java/com/example/newsapp/views/activities/AboutUsActivity.java
@@ -1,12 +1,14 @@
 package com.example.newsapp.views.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.newsapp.R;
+import com.example.newsapp.Utilities;
 
 public class AboutUsActivity extends AppCompatActivity {
 
@@ -18,5 +20,15 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        setupViews();
+    }
+
+    private void setupViews() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Utilities.applyFontForAViewGroup(toolbar,this);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 }
