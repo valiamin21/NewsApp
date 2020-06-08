a38529f80dd41668371d5134da80be5850378c31
Amin <valiamin21@gmail.com>
2020-06-08 15:15:32 +0430

    modifying todo list

--- app/src/main/java/com/example/newsapp/Utilities.java
+++ app/src/main/java/com/example/newsapp/Utilities.java
@@ -30,9 +30,9 @@ public class Utilities {
     }
 
 
-    // TODO: 5/10/20 using ViewPager and TabView in MainActivity
     // TODO: 5/10/20 offline mode handling (local database)
     // TODO: 5/10/20 making library for typeface management
     // TODO: 6/7/20 modify date showing implementation
     // TODO: 6/7/20 making categories activity
+    // TODO: 6/8/20 connection error handling and empty category handling
 }
